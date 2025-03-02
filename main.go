1387080d3b634317c0d4358784c21999a2a3a53d
onMac <cloudonly21@gmail.com>
2025-03-02 20:51:32 +0900

    change scanPort func to return bool, print msg at main

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	}
 	host := os.Args[1]
 
-	ports := []int{20, 21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 443, 445, 993, 995, 1723, 3306, 3389, 5000, 5432, 8080, 8443}
+	ports := []int{22, 80, 443, 8080, 12345}
 
 	var wg sync.WaitGroup
 
@@ -25,7 +25,11 @@ func main() {
 		wg.Add(1)
 		go func(port int) {
 			defer wg.Done()
-			scanPort(host, port)
+			if isOpen := scanPort(host, port); isOpen {
+				fmt.Printf("%-5d : Open\n", port)
+			} else {
+				fmt.Printf("%-5d : Closed\n", port)
+			}
 		}(port)
 	}
 
@@ -39,13 +43,12 @@ func getUsage() string {
 	return msg
 }
 
-func scanPort(host string, port int) {
+func scanPort(host string, port int) bool {
 	address := net.JoinHostPort(host, strconv.Itoa(port))
 	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
 	if err != nil {
-		fmt.Printf("%d: Closed\n", port)
-		return
+		return false
 	}
 	defer conn.Close()
-	fmt.Printf("%d: Open\n", port)
+	return true
 }
